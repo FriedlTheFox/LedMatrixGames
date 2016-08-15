8e1b72f537ba56787256fe752fb511c14b9b1678
Matthias <friedlthefox@gmail.com>
2016-08-15 17:53:36 +0200

    serial connection
    first serial communication is working fine :+1:

--- application/GameOfLife/GameOfLife.py
+++ application/GameOfLife/GameOfLife.py
@@ -9,7 +9,7 @@
 #  */
 
 from GameField import GameField
-import time
+import time, serial
 
 # Implementation of the game of life.
 class GameOfLife():
@@ -24,10 +24,28 @@ class GameOfLife():
                        [0, 0, 0, 0, 0, 0, 0, 0],
                        [0, 0, 0, 0, 0, 0, 0, 0]]
         gameField = GameField(initialState)
+        ser = serial.Serial(port='COM3',
+                            baudrate=115200,
+                            parity=serial.PARITY_NONE,
+                            stopbits=serial.STOPBITS_ONE,
+                            bytesize=serial.EIGHTBITS
+                            )
+        time.sleep(2)
         while True:
             gameField.calculateNextRound()
-            time.sleep(1)
+            time.sleep(0.05)
+            x = 0
+            y = 0
+            currentField = gameField.getCurrentField()
+            for line in currentField:
+                for pixel in line:
+                    color = pixel * 50
+                    ser.write("%s,%s,0,%s,0\n" % (x,y,color))
+                    #print "%s,%s,0,%s,0\n" % (x,y,color)
+                    x += 1
+                y += 1
+                x = 0
 
     if __name__ == "__main__":
         main()
-            
+        ser.close()
